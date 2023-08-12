01b1965f80b7c85d00b81ec0d34adbc79ad43a4e
forsythet@gmail.com <forsythet@gmail.com>
2023-08-11 21:31:08 -0500

    added css styling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
+import { useState } from "react";
+
 import Buttons from "./components/Buttons";
 import Table from "./components/Table";
-
+import Banner from "./components/Banner";
+import satData from "./components/satData";
 
 function App() {
+   const [sat, setSat] = useState(satData);
+   const displaySats = [...new Set(satData.map((data) => data.orbitType))];
+
+   const filterByType = (currentType) => {
+      const displaySats = satData.filter((newSatDisplay) => {
+         return newSatDisplay.orbitType === currentType;
+      });
+      setSat(displaySats);
+   };
   return (
-    <div>
-      <Buttons />
-      <Table />
-    </div>
+     <div>
+        <Banner />
+        <Buttons 
+            filterByType={filterByType}
+            setSat={setSat}
+            displaySats={displaySats}
+        />
+        <Table sat={sat} />
+     </div>
   );
 }
 
